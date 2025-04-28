bf5696034c2bf688e2f8d242c9ec8abe9bc85c51
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-04-28 17:17:39 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,39 @@
-<script src="vendor/react.development.js"></script>
-<script src="vendor/react-dom.development.js"></script>
-<script src="data.js"></script>
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <App images={images} visibleItems={visibleItems} finishedItems={finishedItems} />
+);
 
-<script>
-  function App({images = [], visibleItems = [], finishedItems = []}) {
-    return React.createElement(
-      'section',
-      { className: 'game container' },
-      React.createElement(Grid, { images, visibleItems, finishedItems })
-    );
-  }
-
-  function Card({img, text, isVisible, isFinished}) {
-    const className = `card ${isVisible ? 'card-show' : ''} ${isFinished ? 'card-finished' : ''}`;
-    return React.createElement(
-      'li',
-      { className },
-      React.createElement('img', { src: img, width: "204", height: "144", alt: text }),
-      React.createElement('span', null, text)
-    );
-  }
-
-  function Grid({images, visibleItems, finishedItems}) {
-    const cards = images.map((image) =>
-      React.createElement(Card, {
-        key: image.id,
-        img: image.url,
-        text: image.description,
-        isVisible: visibleItems.includes(image.id),
-        isFinished: finishedItems.includes(image.id),
-      })
-    );
-    return React.createElement('ul', { className: 'cards cards-theme-cars' }, cards);
-  }
-
-  const container = document.getElementById('root');
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    React.createElement(App, {
-      images: images,
-      visibleItems: visibleItems,
-      finishedItems: finishedItems,
-    })
+// Корневой компонент приложения
+function App({ images = [], visibleItems = [], finishedItems = [] }) {
+  return (
+    <section className="game container">
+      <Grid images={images} visibleItems={visibleItems} finishedItems={finishedItems} />
+    </section>
   );
-</script>
+}
+
+// Компонент карточки
+function Card({ img, text, isVisible, isFinished }) {
+  const className = `card ${isVisible ? 'card-show' : ''} ${isFinished ? 'card-finished' : ''}`;
+  return (
+    <li className={className}>
+      <img src={img} width="204" height="144" alt="Котик" />
+      <span>{text}</span>
+    </li>
+  );
+}
+
+// Компонент сетки карточек
+function Grid({ images, visibleItems, finishedItems }) {
+  const cards = images.map((image) => (
+    <Card
+      key={image.id}
+      img={image.url}
+      text={image.description}
+      isVisible={visibleItems.includes(image.id)}
+      isFinished={finishedItems.includes(image.id)}
+    />
+  ));
+  return <ul className="cards cards-theme-cars">{cards}</ul>;
+}
