92aed1bd9be76f0edf73de4177fc9294b4e15684
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-05-16 18:09:56 +0400

    Update app.js

--- app.js
+++ app.js
@@ -38,3 +38,16 @@ function Grid({ images, visibleItems, finishedItems }) {
   ));
   return <ul className="cards">{cards}</ul>;
 }
+
+// Компонент каунтер
+function Counter() {
+  const [count, setState] = React.useState(0);
+  
+  return (
+    <section className="container game">
+      <h1>{count}</h1>
+      <button onClick={() => {setState(count + 1)}} className="custom-button">+</button>
+      <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
+    </section>
+  )
+}
