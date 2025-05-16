1671cd9be12e4874e2b43f48a14734c8187ded94
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-05-16 18:33:35 +0400

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ function Counter() {
   const [count, setState] = React.useState(0);
   
   return
-    <h1>{count}</h1>
-    <button onClick={() => {setState(count + 1)}} className="custom-button">+</button>
-    <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => {setState(count + 1)}} className="custom-button">+</button>
+      <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
+    </div>
 }
