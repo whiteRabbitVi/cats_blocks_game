e4f0aa81bba8b456e073371cfde9d11151810b46
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-05-16 18:29:07 +0400

    Update app.js

--- app.js
+++ app.js
@@ -46,9 +46,8 @@ function Grid({ images, visibleItems, finishedItems }) {
 function Counter() {
   const [count, setState] = React.useState(0);
   
-  return (
+  return
     <h1>{count}</h1>
     <button onClick={() => {setState(count + 1)}} className="custom-button">+</button>
     <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
-  )
 }
