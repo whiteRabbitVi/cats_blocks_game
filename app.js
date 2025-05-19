bd7c84860a794eab871c52e9b7f6fd73f0642161
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-05-19 19:05:35 +0400

    Update app.js

--- app.js
+++ app.js
@@ -45,12 +45,18 @@ function Grid({ images, visibleItems, finishedItems }) {
 // Компонент каунтер
 function Counter() {
   const [count, setState] = React.useState(0);
+  const [incCount, setIncCount] = React.useState(0);
   
   return (
     <div className="counter-border">
       <h1>{count}</h1>
       <div className="button-row">
-        <button onClick={() => {setState(count + 1)}} className="custom-button">+</button>
+        <button 
+          onClick={() => {
+          setState(count + 1) 
+          setIncCount(incCount + 1)
+        }} className="custom-button">+ ({incCount})</button>
+    
         <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
       </div>
     </div>
