6b29ceb9abb9deb0d44dcf6277ee620428345507
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-05-26 20:00:43 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,41 +6,71 @@ root.render(
 );
 
 // Компонент приложения
-function App({ images = [], visibleItems = [], finishedItems = [] }) {
+function App({ images = []) {
+  const [stepCount, setStepCount] = React.useState(0)
+  const checkItems = () => {
+    setStepCount((i) => i + 1);
+  }
+  
   return (
     <div>
       <section className="container game">
-        <Grid images={images} visibleItems={visibleItems} finishedItems={finishedItems} />
+        <div className="steps">Шаг {stepsCount}</div>
+        <Grid images={images} checkItems={checkItems} value={stepsCount} />
       </section>
         <Counter />
     </div>
   );
 }
 
+// Компонент сетки карточек
+function Grid({ images, visibleItems, finishedItems }) {
+  const [selectedItems, setSelectedItems] = React.useState([])
+  const handleCardClick = ((id) => {
+    if (selectedItems.includes(id)) {
+      return;
+    }
+    checkItems();
+    setSelectedItems((items) => [...items, id]);
+  }
+  ));
+  
+  return <ul className="cards">{cards}
+    {images.map((item) => (
+      <Card 
+        key={item.id}
+        id={item.id}
+        url={item.url}
+        description={item.description}
+        isFinished={finishedItems.includes(item.id)}
+        isSelected={selectedItems.includes(item.id)}
+        onCardClick={handleCardClick}
+      />
+    ))} 
+    </ul>;
+}
+
 // Компонент карточки
-function Card({ img, text, isVisible, isFinished }) {
-  const className = `card ${isVisible ? 'card-show' : ''} ${isFinished ? 'card-finished' : ''}`;
+function Card({ img, text, checkItems, isSelected, isFinished }) {
+  const className = `card ${isSelected ? 'card-show' : ''} ${isFinished ? 'card-finished' : ''}`;
+  const handleClick = () => {
+        if (isFinished) {
+          return;
+        }
+        onCardClick(id);
+      };
+  
   return (
-    <li className={className}>
-      <img className="img" src={img} width="204" height="144" alt="Котик" />
-      <span>{text}</span>
-    </li>
+    <li className={`card ${className}`} onClick={handleClick}>
+          <img
+            width="204" height="144"
+            src={url}
+            alt={description}
+          />
+        </li>
   );
 }
 
-// Компонент сетки карточек
-function Grid({ images, visibleItems, finishedItems }) {
-  const cards = images.map((image) => (
-    <Card
-      key={image.id}
-      img={image.url}
-      text={image.description}
-      isVisible={visibleItems.includes(image.id)}
-      isFinished={finishedItems.includes(image.id)}
-    />
-  ));
-  return <ul className="cards">{cards}</ul>;
-}
 
 // Компонент каунтер
 function Counter() {
