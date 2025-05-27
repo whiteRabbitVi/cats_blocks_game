13d03c57491751807958c7a70032e0dbbfb610e4
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-05-27 11:42:52 +0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ root.render(
 // Компонент приложения
 function App({ images = [] }) {
   const finishedItems = [];
-  const [stepCount, setStepCount] = React.useState(0)
+  const [stepsCount, setStepCount] = React.useState(0)
   const checkItems = () => {
     setStepCount((i) => i + 1);
   }
