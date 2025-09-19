3a3bda930bf013d015685d67ebdc79ba854a3c9b
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-09-19 13:42:04 +0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,3 +1,23 @@
+// добавление Компонент каунтер в app.js
+// function Counter() {
+//   const [count, setState] = React.useState(0);
+//   const [incCount, setIncCount] = React.useState(0);
+  
+//   return (
+//     <div className="counter-border">
+//       <h1>{count}</h1>
+//       <div className="button-row">
+//         <button 
+//           onClick={() => {
+//           setState(count + 1) 
+//           setIncCount(incCount + 1)
+//         }} className="custom-button">+ (click on plus {incCount} times)</button>
+    
+//         <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
+//       </div>
+//     </div>
+//   )
+// }
 
 // Компонент каунтер
 function Counter() {
