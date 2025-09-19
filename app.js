976276fa5a558c5328fec9365d21346080cc2d07
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-09-19 13:41:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -134,23 +134,3 @@ function Modal({stepsCount, setStepsCount, setFinishedItems}) {
       )
     }
 
-// Компонент каунтер
-function Counter() {
-  const [count, setState] = React.useState(0);
-  const [incCount, setIncCount] = React.useState(0);
-  
-  return (
-    <div className="counter-border">
-      <h1>{count}</h1>
-      <div className="button-row">
-        <button 
-          onClick={() => {
-          setState(count + 1) 
-          setIncCount(incCount + 1)
-        }} className="custom-button">+ (click on plus {incCount} times)</button>
-    
-        <button onClick={() => {setState(count - 1)}} className="custom-button">-</button>
-      </div>
-    </div>
-  )
-}
