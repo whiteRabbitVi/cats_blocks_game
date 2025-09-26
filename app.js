1c189bc02c07c751b9749e8579fab65b1bc9ff7b
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-09-26 22:15:01 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,151 @@
 // Получаем контейнер для рендеринга приложения
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App images={images} />)
+root.render(<App getImages={getImages} results={results} />)
 
 // Компонент приложения
-function App({ images = [] }) {
-  const [finishedItems, setFinishedItems] = React.useState([]);
-  const [stepsCount, setStepsCount] = React.useState(0)
-  
-  const checkItems = (firstItem, secondItem) => {
-      firstItem = images.find(({id}) => id === firstItem);
-      secondItem = images.find(({id}) => id === secondItem);
-      if (firstItem.url === secondItem.url) {
-        setFinishedItems((items) => [...items, firstItem.id, secondItem.id])
-      }
-      setStepsCount((i) => i + 1);
+function App({ results, getImages }) {
+  const [page, setPage] = React.useState(AppRoute.InitialPage);
+  const [result, setResult] = React.useState(0);
+  const [images, setImages] = React.useState([])
+  const [type, setType] = React.useState(null);
+
+  const showResults = (stepsCount) => {
+    setResult(stepsCount);
+    setPage(AppRoute.Results);
     };
 
-  const isWin = finishedItems.length === images.length;
+  const handleReset = () => {
+    setResult(0);
+    setImages([]);
+    setPage(AppRoute.InitialPage);
+    };
+
+  const handleStart = (type) => {
+    setType(type);
+    setImages(getImages(type));
+    setPage(AppRoute.Game);
+  }
   
-  return (
-    <div>
-      <section className="container game">
-        <Progress value={finishedItems.length} max={images.length} stepsCount={stepsCount}/>
-        <Grid 
-          images={images} 
-          finishedItems={finishedItems}
-          checkItems={checkItems} 
-        />
-        {isWin && <Modal 
-          stepsCount={stepsCount}
-          setFinishedItems={setFinishedItems}
-          setStepsCount={setStepsCount}
-      />}
-      </section>
-    </div>
-  )
+  const getPage = (route) => {
+    switch (route) {
+      case AppRoute.InitialPage:
+        return <InitialPage onStart={handleStart} />
+      case AppRoute.Game:
+        return <GamePage images={images} onShowResults={showResults} type={type} />;
+      case AppRoute.Results:
+        return (
+          <ResultsPage
+            stepsCount={result}
+            onResetGame={handleReset}
+            results={results}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+  return getPage(page);
 }
 
+// Функции сравнения карточек и определения победы
+ const useGame = (images) => {
+  const [finishedItems, setFinishedItems] = React.useState([]);
+  const [stepsCount, setStepsCount] = React.useState(0)
+
+   const checkItems = (firstItem, secondItem) => {
+    firstItem = images.find(({id}) => id === firstItem);
+    secondItem = images.find(({id}) => id === secondItem);
+    if (firstItem.url === secondItem.url) {
+      setFinishedItems((items) => [...items, firstItem.id, secondItem.id])
+    }
+    setStepsCount((i) => i + 1);
+  };
+
+   const isWin = finishedItems.length === images.length;
+
+  return {
+    finishedItems,
+    stepsCount,
+    checkItems,
+    isWin
+  };
+ }
+
+// Компонент Стартовая страница
+function InitialPage({onStart}) {
+      const handleStart = (type) => {
+        onStart(type)
+      }
+
+      return (
+              <section className="rules container">
+                <h2>Добро пожаловать!</h2>
+                <p>Memory — игра для тренировки визуальной памяти</p>
+                <div className="rules-panel">
+                  <h3>Правила игры</h3>
+                  <ul className="rules-list">
+                    <li>В наборе есть множество карточек – по две штуки с одним и тем же рисунком.</li>
+                    <li>Нужно разложить карточки «рубашкой» вверх на столе, а затем переворачивать по две.</li>
+                    <li>Если они совпадают – игрок забирает их и получает ещё один ход.</li>
+                  </ul>
+                </div>
+                <button className="ico-button ico-button-cats" type="button" onClick={() => handleStart("cats")}>Котики</button>
+                <button className="ico-button ico-button-flowers" type="button" onClick={() => handleStart("flowers")}>Цветочки</button>
+                <button className="ico-button ico-button-cars" type="button"onClick={() => handleStart("cars")}>Машины</button>
+              </section>
+      );
+    }
+
+// Родительский компонент для прогресс-бара, сетки, модалки результата
+    function GamePage({images = [], onShowResults, type}) {
+      const {
+        finishedItems,
+        stepsCount,
+        checkItems,
+        isWin,
+      } = useGame(images);
+
+      const handleResultsClick = () => {
+        onShowResults(stepsCount);
+      };
+
+      return (
+        <section className="game container">
+          <Progress value={finishedItems.length / 2} max={images.length / 2} />
+          <div className="steps">Шаг {stepsCount}</div>
+          <Grid
+            images={images}
+            finishedItems={finishedItems}
+            checkItems={checkItems}
+            type={type}
+          />
+          {isWin && (
+            <Modal>
+              <h3 className="modal-caption">Победа!</h3>
+              <p className="modal-description">Теперь давайте узнаем результаты этой партии</p>
+              <button className="button modal-button" type="button" onClick={handleResultsClick}>Показать результаты</button>
+            </Modal>
+          )}
+        </section>
+      );
+    }
+
 // Компонент прогресса
-    function Progress({ value, max, stepsCount }) {
+    function Progress({ value, max }) {
       return (
       <>
             <div className="progress-wrapper">
               <div className="progress" style={{ width: `${(value / max) * 100}%` }}></div>
             </div>
-
-            <p className="progress-description">
-              Открыто <span>{value / 2}</span> / <span>{max / 2}</span>
-            </p>
-
+              <p className="progress-description">Открыто {value} / {max}</p>
             <div className="steps">Шаг {stepsCount}</div>
         </>
       );
     }
 
 // Компонент сетки карточек
-  function Grid({images, finishedItems, checkItems}) {
+  function Grid({images = [], finishedItems, checkItems, type}) {
     const [selectedItems, setSelectedItems] = React.useState([]);
     
     const handleCardClick = (id) => {
@@ -79,18 +168,18 @@ function App({ images = [] }) {
         }
       };
 
-    return <ul className="cards">
-      {images.map((item) => (
-              <Card
-                  key={item.id}
-                  id={item.id}
-                  url={item.url}
-                  description={item.description}
-                  isFinished={finishedItems.includes(item.id)}
-                  isSelected={selectedItems.includes(item.id)}
-                  onCardClick={handleCardClick}
-              />
-      ))}
+    return <ul className={`cards cards-theme-${type}`}>
+        {images.map((item) => (
+                <Card
+                    key={item.id}
+                    id={item.id}
+                    url={item.url}
+                    description={item.description}
+                    isFinished={finishedItems.includes(item.id)}
+                    isSelected={selectedItems.includes(item.id)}
+                    onCardClick={handleCardClick}
+                />
+        ))}
       </ul>
   }
 
@@ -116,11 +205,15 @@ function Card({ url, description, id, isSelected, isFinished, onCardClick }) {
 }
 
 // Компонент модальное окно
-function Modal({stepsCount, setStepsCount, setFinishedItems}) {
-      const handleModalClick = () => {
-        setFinishedItems([]);
-        setStepsCount(0);
-      }
+function Modal({children}) {
+      return (
+        <div className="modal">
+          <div className="modal-box">
+            {children}
+          </div>
+        </div>
+      );
+    }
 
       return (
         <div className="modal">
